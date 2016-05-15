e0c2d1ad76841c6bb907d8bcee2176238a47a0fa
Tirso Peguero <tito.peguero@gmail.com>
2016-05-15 14:19:02 -0700

    use env var to store github url

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,14 +1,15 @@
 defmodule Issues.GithubIssues do
   @user_agent [ {"User-agent", "Elixir tito.peguero@gmail.com"} ]
+  @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, project) do
     issues_url(user, project)
-      |> HTTPoison.get(@user_agent)
-      |> handle_response
+    |> HTTPoison.get(@user_agent)
+    |> handle_response
   end
 
   def issues_url(user, project) do
-    "https://api.github.com/repos/#{user}/#{project}/issues"
+    "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
   def handle_response({ :ok, %{status_code: 200, body: body} }) do
